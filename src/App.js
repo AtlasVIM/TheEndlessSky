76587b250757850c3b1551bc04e41d4b2a97f468
AtlasVIM <caldasmartim@gmail.com>
2024-01-15 22:34:02 +0000

    added images, glow on hover and tooltips

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function showNordansk() {
+  alert('test');
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "container">
+      <div id="background-map"></div>
+      <div class="tooltip" id="nordansk" onClick={showNordansk} border="0"><span class="tooltiptext">Nordansk</span></div>  
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
